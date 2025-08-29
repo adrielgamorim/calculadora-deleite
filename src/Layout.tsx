a3b693825eea117c28fcfe9c99d6393222397c74
Adrielgamorim <agimenescomercial@gmail.com>
2025-08-28 22:22:54 -0300

    Centered Loading component

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -11,7 +11,7 @@ export function Layout() {
   const { user, loading, signInWithGoogle, signOut } = useAuth();
 
   if (loading) {
-    return <div><Loading /></div>;
+    return <Loading />;
   }
 
   const whitelist: string[] = import.meta.env.VITE_WHITELIST.split(",");
