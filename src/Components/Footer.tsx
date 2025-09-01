d04db29ae2eb53997aa20ccf8d40d82a4c3fa157
Adrielgamorim <agimenescomercial@gmail.com>
2025-09-01 00:03:57 -0300

    Now storing only references of ingredients and bundles instead of whole object - Removed warning when edting ingredients - Ingredients and Bundles now displayed alphabetically (v-beta-0.3)

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer style={{ fontSize: "0.7rem" }}>
       <p>&copy; 2025 Calculadora D'Leite. Todos os direitos reservados.</p>
-      <p>Versão beta 0.4</p>
+      <p>Versão beta 0.3</p>
     </footer>
   );
 }
